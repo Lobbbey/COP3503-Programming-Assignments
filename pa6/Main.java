63c8f5260b65cfbd6729c9fe2535d67f2252c1ee
Lobbbey <joshuatylerbandy@gmail.com>
2024-07-25 16:08:00 -0400

    Work done to pa6

--- pa6/Main.java
+++ pa6/Main.java
@@ -8,8 +8,30 @@ import java.util.*;
 
 public class Main{
 
+    //Global Declarations
+    public static int numStudents, totalProb = 0;
 
     public static void main(String[] args){
+        //Declarations
+        Scanner scanIn = new Scanner(System.in);
+
+        numStudents = scanIn.nextInt();//Get number of students
+
+        //Create arrays based on num of students
+        int[] row1 = new int[numStudents];
+        int[] row2 = new int[numStudents];
+
+        //Collect input for each row
+        for(int i = 0; i < numStudents; i++){
+            row1[i] = scanIn.nextInt();
+        }
+
+        for(int i = 0; i < numStudents; i++){
+            row2[i] = scanIn.nextInt();
+        }
+
         
-    }
+
+        scanIn.close();
+    }    
 }
